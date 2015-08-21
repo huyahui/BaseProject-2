56c381ae5c13c66438abb632d7fd755010961e3c
zhaoquanhong <amateur.w7@outlook.com>
2015-08-21 15:22:43 +0800

    add an empty project

---
+++ BaseProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BaseProject
+//
+//  Created by amateur on 8/21/15.
+//  Copyright © 2015 XA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ BaseProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseProject
+//
+//  Created by amateur on 8/21/15.
+//  Copyright © 2015 XA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
